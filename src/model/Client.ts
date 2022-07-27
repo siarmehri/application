cb69d3fb90acf8d07500d9c98ae52ba0296ee31a
Ahmad Siar Mehri <siarmehri@1stlocate.com>
2022-07-27 21:40:44 +0100

    Added Save, FindOne, and UpdateOrCreate function to Client

--- src/model/Client.ts
+++ src/model/Client.ts
@@ -13,6 +13,7 @@ import {
   Scopes,
   HasMany,
 } from 'sequelize-typescript';
+import { Transaction } from 'sequelize';
 import { Address } from './Address';
 import { ClientContact } from './ClientContact';
 import { EmailAddress } from './EmailAddress';
@@ -169,12 +170,49 @@ export class Client extends Model<Client> {
   clientContacts: [ClientContact];
 
   @HasMany(() => Address)
-  addresses: [Address];
+  addresses: Address[];
 
   @HasMany(() => PhoneNumber)
-  phonenumbers: [PhoneNumber];
+  phoneNumbers: PhoneNumber[];
+
   @HasMany(() => EmailAddress)
-  emails: [EmailAddress];
+  emails: EmailAddress[];
+
   @HasMany(() => Website)
-  websites: [Website];
+  websites: Website[];
+
+  // Ashraf Khan please create IClient interfaces to make your life easier.
+  public static async SaveClient(clientData: any, transaction: Transaction) {
+    try {
+      return Promise.resolve(await Client.create(
+        clientData,
+        { transaction: transaction }));
+    } catch (err) {
+      console.log((err as any).message);
+      return Promise.reject(err);
+    }
+  }
+  static async FindOne(clientData: any, transaction: Transaction) {
+    try {
+      return Promise.resolve(await Client.findOne({
+        where: {
+          id: clientData.id
+        }, transaction: transaction
+      }));
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  }
+  public static async UpdateOrCreate(clientData: any, transaction: Transaction) {
+    try {
+      const client = await this.FindOne(clientData, transaction);
+      return (!client) ?
+        Promise.resolve(await Client.SaveClient(clientData, transaction)) :
+        Promise.resolve(Client.update(clientData, {
+          where: { id: clientData.id }, transaction: transaction
+        }));
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  }
 }
